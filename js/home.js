fb5df5363b1eb10b86c7fd4253665f4e66092c9a
WebXR-CrazyPencilz <3dactz@gmail.com>
2026-06-08 19:08:20 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -17,14 +17,14 @@ window.HomeModule = (function () {
 
   // ─── CAROUSEL IMAGES ─────────────────────────────────────────────
   const IMAGES = [
-    { src: 'https://ik.imagekit.io/pwzaetheh/Home/a.jpg', label: 'View 1' },
-    { src: 'https://ik.imagekit.io/pwzaetheh/Home/b.jpg', label: 'View 2' },
+    { src: 'https://ik.imagekit.io/pwzaetheh/Home2/a.jpg', label: 'View 1' },
+    { src: 'https://ik.imagekit.io/pwzaetheh/Home2/b.jpg', label: 'View 2' },
     { src: 'https://ik.imagekit.io/pwzaetheh/Home/c.jpg', label: 'View 3' },
     { src: 'https://ik.imagekit.io/pwzaetheh/Home/d.jpg', label: 'View 4' },
     { src: 'https://ik.imagekit.io/pwzaetheh/Home/e.jpg', label: 'View 5' },
     { src: 'https://ik.imagekit.io/pwzaetheh/Home/g.jpg', label: 'View 6' },
     { src: 'https://ik.imagekit.io/pwzaetheh/Home/h.jpg', label: 'View 7' },
-    { src: 'https://ik.imagekit.io/pwzaetheh/Home/i.jpg', label: 'View 7' },
+    { src: 'https://ik.imagekit.io/pwzaetheh/Home2/i.jpg', label: 'View 7' },
     { src: 'https://ik.imagekit.io/pwzaetheh/Home/j.jpg', label: 'View 8' },
     { src: 'https://ik.imagekit.io/pwzaetheh/Home/l.jpg', label: 'View 9' },
     { src: 'https://ik.imagekit.io/pwzaetheh/Home/m.jpg', label: 'View 10' },
