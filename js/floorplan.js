acafdc547344344b5108c6f49dbaf13611115ce9
WebXR-CrazyPencilz <3dactz@gmail.com>
2026-06-09 14:25:12 +0530

    Update floorplan.js

--- js/floorplan.js
+++ js/floorplan.js
@@ -23,7 +23,7 @@ window.FloorplanModule = (function () {
       {
         id: 'tower-A', label: 'Tower A',
         // Top-right building block on the sitemap
-        points: '58,20 68,20 68,52 52,52 52,42 57,42 57,32 52,32 52,26 58,26',
+        points: '78,20 68,20 68,52 52,52 52,42 57,42 57,32 52,32 52,26 78,26',
       },
 
       //{
